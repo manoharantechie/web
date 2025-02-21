009ca61bb6660cd3a790da7f62257d694f9a4e9e
manoharantechie <manopncity@gmail.com>
2025-02-21 15:44:27 +0530

    final commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "20413606"
+    serviceWorkerVersion: "4202539379"
   }
 });
